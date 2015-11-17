8ffe7cdcdb6e77573cdfb40d7a0407ea67e9d937
Trevor Petersen <tpetersen125@yahoo.com>
2015-11-17 14:24:52 -0700

    I have added a scene for testing touch inputs, a prefab cube for testing the touch inputs, and a few scripts that manage the touches.

---
+++ GumBot/Assets/Modules/GUI/GUI Scripts/buttonTouch.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonTouch : MonoBehaviour {
+
+	public Color defaultColor;
+	public Color selectedColor;
+	public Color swipeColor;
+	private Material mat;
+
+	void Start(){
+		mat = GetComponent<Renderer>().material;
+	}
+
+	void OnTouchDown(){
+		mat.color = selectedColor;
+		//print ("It's working!");
+	}
+
+	void OnTouchUp(){
+		mat.color = defaultColor;
+	}
+
+	void OnTouchStay(){
+		mat.color = selectedColor;
+	}
+
+	void OnTouchMove(){
+		mat.color = swipeColor;
+	}
+
+	void OnTouchExit(){
+		mat.color = defaultColor;
+	}
+
+	void Update(){
+	}
+}
