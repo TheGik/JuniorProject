8b1bdcab724ca81e73489876dcd4fb947d98beff
Jeremy Graham <ss2jeremy@gmail.com>
2015-11-18 20:26:03 -0700

    Added shooting to Player

---
+++ GumBot/Assets/Modules/Player/Scripts/CharacterShot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterShot : MonoBehaviour {
+
+ 
+    public GameObject bullet;
+    public Transform shootingPoint;
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(bullet, shootingPoint.position, shootingPoint.rotation);
+
+        
+        }
+
+        }
+}
---
+++ GumBot/Assets/Modules/Player/Scripts/bulletMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletMovement : MonoBehaviour {
+    public float shotVelocity;
+    public Rigidbody rb;
+    public CharacterMotor cm;
+    public GameObject TheDude;
+    public float lifeDuration;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        cm = FindObjectOfType<CharacterMotor>();
+        if (cm.facingRight == true)
+        {
+            rb.velocity = transform.TransformDirection (new Vector3(shotVelocity, 0, 0));
+        }
+        if (cm.facingRight == false)
+        {
+            rb.velocity = transform.TransformDirection(new Vector3(-shotVelocity, 0, 0));
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("Destroybullet");
+    }
+    public IEnumerator Destroybullet()
+    {
+        yield return new WaitForSeconds(lifeDuration);
+        Destroy(gameObject);
+    }
+}
