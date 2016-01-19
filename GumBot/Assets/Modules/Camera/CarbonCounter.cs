462940e8249687eb69c24f41233f09daa7197372
Jeremy Graham <ss2jeremy@gmail.com>
2016-01-19 12:31:41 -0700

    no message

--- GumBot/Assets/Modules/Camera/CarbonCounter.cs
+++ GumBot/Assets/Modules/Camera/CarbonCounter.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class CarbonCounter : MonoBehaviour {
 
     public static int carbontotal = 0;
+    public static int irontotal = 0;
+    public static int silicatotal = 0;
     // Use this for initialization
     void Start () {
 	
@@ -15,7 +17,17 @@ public class CarbonCounter : MonoBehaviour {
 	}
     void OnGUI()
     {
-        string carbontext = "Carbon Count: " + carbontotal;
-        GUI.Box(new Rect(Screen.width - 150, 20, 130, 20), carbontext);
+        {
+            string carbontext = "Carbon Count: " + carbontotal;
+            GUI.Box(new Rect(Screen.width - 150, 20, 130, 20), carbontext);
+        }
+        {
+            string irontext = "Iron Count: " + irontotal;
+            GUI.Box(new Rect(Screen.width - 150, 45, 130, 20), irontext);
+        }
+        {
+            string silicatext = "Silica Count: " + silicatotal;
+            GUI.Box(new Rect(Screen.width - 150, 70, 130, 20), silicatext);
+        }
     }
 }
--- GumBot/Assets/Modules/Enemies/Scripts/AI/enemyShotMovement.cs
+++ GumBot/Assets/Modules/Enemies/Scripts/AI/enemyShotMovement.cs
@@ -3,30 +3,29 @@ using System.Collections;
 
 public class enemyShotMovement : MonoBehaviour {
 
-    public float enemyShotVelocity;
+    public float enemyShotVelocityX;
+    public float enemyShotVelocityY;
     public Rigidbody rb;
     public float lifeDuration;
     private Attractor Core;
     public float Xpos;
     public float Ypos;
     public float radius;
+   
+
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.TransformDirection(new Vector3(-enemyShotVelocity, 0, 0));
+
+            rb.velocity = transform.TransformDirection(new Vector3(enemyShotVelocityX, enemyShotVelocityY, 0));
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 Xpointer = (Core.transform.position - transform.position).normalized;
-        Xpointer.z = 0;
-        float temp = Xpointer.x;
-        Xpointer.x = -Xpointer.y;
-        Xpointer.y = temp;
-
-        transform.position = transform.position + ((Xpointer * -1) / 5);
+ 
 
         StartCoroutine("Destroybullet");
     }
